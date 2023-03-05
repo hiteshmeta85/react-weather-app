a9295a44082612ac720bb99ca2b6d33a5d00eaff
Mubin Ansari <mubinansari910@gmail.com>
2023-03-06 02:27:33 +0530

    09-Add degree celcius icon to summary temp

--- src/components/WeatherSection/Summary.jsx
+++ src/components/WeatherSection/Summary.jsx
@@ -1,19 +1,26 @@
 import React from 'react';
 
-const Summary = ({day, date, location, icon, temperature, type}) => {
+const Summary = ({ day, date, location, icon, temperature, type }) => {
   return (
     <div className='weather__summary__section'>
       <div>
         <p className='weather__day'>{day}</p>
         <p className='weather__date'>{date}</p>
         <div className='weather__location'>
-          <img src='/src/assets/location-marker.svg' alt='marker icon'/>
+          <img src='/src/assets/location-marker.svg' alt='marker icon' />
           <p>{location}</p>
         </div>
       </div>
       <div>
-        <img className='weather__icon' src={`https://openweathermap.org/img/w/${icon}.png`} alt='weather icon'/>
-        <p className='weather__temp'>{temperature}</p>
+        <img
+          className='weather__icon'
+          src={`https://openweathermap.org/img/w/${icon}.png`}
+          alt='weather icon'
+        />
+        <p className='weather__temp'>
+          {temperature}
+          <sup> o</sup>C
+        </p>
         <p className='weather__type'>{type}</p>
       </div>
     </div>
