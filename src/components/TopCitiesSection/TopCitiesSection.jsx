63c4d9876573b09dfb9b0d0e406bea776ba1625f
Mubin Ansari <mubinansari910@gmail.com>
2023-03-06 02:50:18 +0530

    13-make top cities section dynamic

--- src/components/TopCitiesSection/TopCitiesSection.jsx
+++ src/components/TopCitiesSection/TopCitiesSection.jsx
@@ -1,14 +1,40 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { fetchWeather } from '../../api/index.js';
 import WeatherCard from './WeatherCard.jsx';
 
+const TOP_CITIES = ['Paris', 'New York', 'Tokyo', 'London', 'Dubai'];
+
 const TopCitiesSection = () => {
+  const [weatherData, setWeatherData] = useState();
+
+  const fetchWeathersHandler = async () => {
+    try {
+      let data = [];
+      for (const city of TOP_CITIES) {
+        const cityData = await fetchWeather(city);
+        data.push(cityData);
+      }
+      setWeatherData(data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchWeathersHandler();
+  }, []);
+
   return (
     <div className='top__cities__section'>
-      <WeatherCard city='Paris' />
-      <WeatherCard city='New York' />
-      <WeatherCard city='Tokyo' />
-      <WeatherCard city='London' />
-      <WeatherCard city='Dubai' />
+      {TOP_CITIES.map((city, index) => {
+        return (
+          <WeatherCard
+            key={index}
+            city={city}
+            weatherData={weatherData && weatherData[index]}
+          />
+        );
+      })}
     </div>
   );
 };
--- src/components/TopCitiesSection/WeatherCard.jsx
+++ src/components/TopCitiesSection/WeatherCard.jsx
@@ -1,17 +1,25 @@
 import React from 'react';
 
-const WeatherCard = () => {
+const WeatherCard = ({ city, weatherData }) => {
+  if (!weatherData) {
+    return <p className='top__city__card'>Loading...</p>;
+  }
+
   return (
     <div className='top__city__card'>
       <div>
         <img
           className='top__city__weather__icon'
-          src='/sun.svg'
+          src={`http://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`}
           alt='weather icon'
         />
-        <p className='top__city__weather__temp'>29&#8451;</p>
-        <p className='top__city__weather__type'>Sunny</p>
-        <p className='top__city__card__city'>Mumbai, India</p>
+        <p className='top__city__weather__temp'>
+          {weatherData.main.temp}&#8451;
+        </p>
+        <p className='top__city__weather__type'>
+          {weatherData.weather[0].main}
+        </p>
+        <p className='top__city__card__city'>{city}</p>
       </div>
     </div>
   );
