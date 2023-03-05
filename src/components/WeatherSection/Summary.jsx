81e64d9174631bd67c764552f659ddcce62312f1
Hitesh Meta <meta.hitesh85@gmail.com>
2023-03-05 23:56:33 +0530

    05-Add Api fetching to summary card

--- src/components/WeatherSection/Summary.jsx
+++ src/components/WeatherSection/Summary.jsx
@@ -1,20 +1,20 @@
 import React from 'react';
 
-const Summary = () => {
+const Summary = ({day, date, location, icon, temperature, type}) => {
   return (
     <div className='weather__summary__section'>
       <div>
-        <p className='weather__day'>Friday</p>
-        <p className='weather__date'>3 March 2022</p>
+        <p className='weather__day'>{day}</p>
+        <p className='weather__date'>{date}</p>
         <div className='weather__location'>
           <img src='/src/assets/location-marker.svg' alt='marker icon'/>
-          <p>Mumbai, India</p>
+          <p>{location}</p>
         </div>
       </div>
       <div>
-        <img className='weather__icon' src='/src/assets/sun.svg' alt='weather icon'/>
-        <p className='weather__temp'>29&#8451;</p>
-        <p className='weather__type'>Sunny</p>
+        <img className='weather__icon' src={`https://openweathermap.org/img/w/${icon}.png`} alt='weather icon'/>
+        <p className='weather__temp'>{temperature}</p>
+        <p className='weather__type'>{type}</p>
       </div>
     </div>
   );
--- src/components/WeatherSection/WeatherSection.jsx
+++ src/components/WeatherSection/WeatherSection.jsx
@@ -1,12 +1,47 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import Summary from "./Summary.jsx";
 import Form from "./Form.jsx";
 import Stats from "./Stats.jsx";
 
 const WeatherSection = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [didWeatherDataLoad, setDidWeatherDataLoad] = useState(false);
+  const [weatherData, setWeatherData] = useState({});
+  const [currentLocation, setCurrentLocation] = useState('Mumbai');
+
+  const fetchData = async (location) => {
+    setIsLoading(true);
+    await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=fcc8de7015bbb202209bbf0261babf4c`)
+      .then(response => response.json())
+      .then(data => {
+        setWeatherData(data);
+        setIsLoading(false);
+        setDidWeatherDataLoad(true);
+      })
+      .catch(error => console.log(error));
+  };
+
+  useEffect(() => {
+    fetchData(currentLocation);
+  }, []);
+
+  const today = new Date();
+  const day = today.toLocaleString('default', {weekday: 'long'});
+  const date = today.toLocaleString('default', {month: 'long'}) + ' ' + today.getDate();
+
   return (
     <div className='weather'>
-      <Summary/>
+      {isLoading ? <p>Loading...</p> : didWeatherDataLoad ?
+        <Summary
+          date={date}
+          day={day}
+          icon={weatherData.weather[0].icon}
+          location={weatherData.name}
+          temperature={weatherData.main.temp}
+          type={weatherData.weather[0].main}
+        />
+        : <p>Weather data not available</p>
+      }
       <div className='weather__right__section'>
         <Stats/>
         <Form/>
