4474177d9937c98fc943f859a49a047f198f1e06
Mubin Ansari <mubinansari910@gmail.com>
2023-03-10 21:29:24 +0530

    modularize WeatherSection code

--- src/components/WeatherSection/WeatherSection.jsx
+++ src/components/WeatherSection/WeatherSection.jsx
@@ -1,67 +1,48 @@
-import React, {useEffect, useState} from 'react';
-import {fetchWeather} from '../../api/index.js';
-import Form from './Form.jsx';
-import Stats from './Stats.jsx';
-import Summary from './Summary.jsx';
+import { useEffect, useState } from 'react';
+import { fetchWeather } from '../../api';
+import Form from './Form';
+import Stats from './Stats';
+import Summary from './Summary';
 
 const TODAY = new Date();
-const DAY = TODAY.toLocaleString('default', {weekday: 'long'});
-const DATE = TODAY.toLocaleString('default', {month: 'long'}) + ' ' + TODAY.getDate();
+const DAY = TODAY.toLocaleString('default', { weekday: 'long' });
+const DATE =
+  TODAY.toLocaleString('default', { month: 'long' }) + ' ' + TODAY.getDate();
 
 const WeatherSection = () => {
   const [isLoading, setIsLoading] = useState(true);
-  const [didWeatherDataLoad, setDidWeatherDataLoad] = useState(false);
-  const [weatherData, setWeatherData] = useState();
+  const [weatherData, setWeatherData] = useState(null);
   const [currentLocation, setCurrentLocation] = useState('Panvel');
 
   const fetchWeatherHandler = async () => {
     setIsLoading(true);
-    try {
-      const data = await fetchWeather(currentLocation);
-      setWeatherData(data);
-      setIsLoading(false);
-      setDidWeatherDataLoad(true);
-    } catch (error) {
-      console.error(error);
-    }
+    const data = await fetchWeather(currentLocation);
+    setWeatherData(data.cod === 200 ? data : null);
+    setIsLoading(false);
   };
 
   useEffect(() => {
     fetchWeatherHandler();
   }, []);
 
-  let leftSectionContent;
-  let rightSectionContent;
-
-  if (isLoading || !didWeatherDataLoad) {
-    const textContent = isLoading ? 'Loading...' : 'Weather data not available';
-    leftSectionContent = (
-      <p className='weather__summary__section'>{textContent}</p>
-    );
-    rightSectionContent = (
-      <p className='weather__stat__section'>{textContent}</p>
-    );
-  }
-
-  if (!isLoading && didWeatherDataLoad) {
-    leftSectionContent = (
-      <Summary
-        date={DATE}
-        day={DAY}
-        icon={weatherData.weather[0].icon}
-        location={weatherData.name}
-        temperature={weatherData.main.temp}
-        type={weatherData.weather[0].main}
-      />
-    );
-    rightSectionContent = <Stats weatherData={weatherData}/>;
-  }
-
   return (
     <div className='weather'>
-      {leftSectionContent}
+      {isLoading || !weatherData ? (
+        <p className='weather__summary__section'>
+          {isLoading ? 'Loading...' : 'Weather data not available'}
+        </p>
+      ) : (
+        <Summary
+          date={DATE}
+          day={DAY}
+          icon={weatherData.weather[0].icon}
+          location={weatherData.name}
+          temperature={weatherData.main.temp}
+          type={weatherData.weather[0].main}
+        />
+      )}
       <div className='weather__right__section'>
-        {rightSectionContent}
+        {!isLoading && weatherData && <Stats weatherData={weatherData} />}
         <Form
           currentLocation={currentLocation}
           setCurrentLocation={setCurrentLocation}
